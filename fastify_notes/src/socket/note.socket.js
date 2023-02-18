5e8ed7d0ee1627b6197aaced5831c82f25f1dff1
SrCristian2 <cristiancros12@gmail.com>
2023-02-18 00:17:26 -0500

    notes

---
+++ fastify_notes/src/socket/note.socket.js
@@ -0,0 +1,40 @@
+
+import { noteModel } from "../models/notes.model.js";
+
+export const socket = (fastify) => {
+
+  fastify.io.on("connection", (socket) => {
+    console.log(`usuario conectado: ${socket.id}`);
+
+    const getNotes = async () => {
+      const notes = await noteModel.find();
+  
+      fastify.io.emit("server:getNotes", notes);
+    };
+
+    getNotes();
+
+    socket.on("client:addNote", async (note) => {
+      await noteModel.create(note);
+      getNotes();
+    });
+
+    socket.on("client:updateNote", async (note) => {
+      await noteModel.findByIdAndUpdate({_id: note._id }, note);
+      getNotes();
+    });
+    socket.on("client:deleteNote", async (id) => {
+      await noteModel.findByIdAndDelete(id);
+      getNotes();
+    });
+
+    socket.on("client:getNote", async (id) => {
+      const note = await noteModel.findById(id);
+      fastify.io.emit("server:getNote", note);
+    });
+
+    socket.on("disconnect", () => {
+      console.log(`usuario desconectado ${socket.id}`);
+    });
+  });
+};
