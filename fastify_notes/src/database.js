4cdc370761c63d5a616b8d513357f615786c55ec
SrCristian2 <cristiancros12@gmail.com>
2023-02-28 10:16:35 -0500

    up

--- fastify_notes/src/database.js
+++ fastify_notes/src/database.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 mongoose.set("strictQuery", false);
 const uri =
-  "mongodb+srv://Cristian:515837020l@cluster0.ywnwo5p.mongodb.net/notes_socket";
+  "";
 
 export const connectDb = async () => {
   try {
